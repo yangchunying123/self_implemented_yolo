c6b08a9818e605e1431ab442d98516caf8577def
mac <1181378038@qq.com>
2025-03-03 22:55:06 +0800

    modify it at home

--- origin_yolov1/models/loss.py
+++ origin_yolov1/models/loss.py
@@ -60,19 +60,17 @@ class Loss(nn.Module):
         batch_size, S, _, N = pred_tensor.size(0), pred_tensor.size(1), pred_tensor.size(2), pred_tensor.size(3) 
         B = (N - self.C) // 5 
         obj_mask = (target_tensor[..., 4] == 1).unsqueeze(-1).expand_as(target_tensor)
-        no_obj_mask = (target_tensor[..., 4] == 0).unsqueeze(-1).expand_as(target_tensor) # actually, even one demension is label as "contains obj", but those predicted boxes that
+        no_obj_mask = (target_tensor[..., 4] < 1).unsqueeze(-1).expand_as(target_tensor) # actually, even one demension is label as "contains obj", but those predicted boxes that
                                                                                          # do not have largest iou with gt should also be regarded as no_obj. this should be dealed 
                                                                                          # with later
-        gt_obj = target_tensor[obj_mask].view(-1, N).contiguous()     #(obj_num, N)
-        pre_obj = pred_tensor[obj_mask].view(-1, N).contiguous()     #(obj_num, N)
-        obj_num = gt_obj.size(0)  # num of objects contains in a image/batch
-
-        gts =  gt_obj[ : , : 5].contiguous()     #(obj_num, 5)
-        preds_posi = torch.zeros(obj_num, 5).cuda()     #(obj_num, 5)
-        
         my_gt, my_preds = self.txty2xy(target_tensor, pred_tensor)
         my_gt = my_gt[obj_mask].view(-1, N)             #(obj_num, N)
         my_preds = my_preds[obj_mask].view(-1, N)       #(obj_num, N)
+        obj_num = my_gt.size(0)  # num of objects contains in a image/batch
+        
+        gt_objs =  target_tensor[obj_mask].view(-1, N).contiguous()     #(obj_num, 5)
+        pred_objs = pred_tensor[obj_mask].view(-1, N).contiguous()
+        preds_posi = torch.zeros(obj_num, 5).cuda()    #(obj_num, 5)
 
         extra_conf = []
         for  i in range(obj_num):
@@ -87,21 +85,21 @@ class Loss(nn.Module):
             iou = self.compute_iou(gt_box, pred_box)
             iou_value, index = iou.max(1)    
             iou_value, index = iou_value.item(), index.item()  
-            gts[i, 4] = iou_value
+            gt_objs[i, 4] = iou_value
             for b in range(B):
                 if b != index:
-                    extra_conf.append(pre_obj[i, 5 * b + 4].item())
+                    extra_conf.append(pred_objs[i, 5 * b + 4].item())
                 else:
-                    preds_posi[i] = pre_obj[i, 5 * b : 5 * b + 5]
+                    preds_posi[i] = pred_objs[i, 5 * b : 5 * b + 5]
         
         pre_nega_conf = pred_tensor[no_obj_mask].contiguous().view(-1, N)[ :, : 5 * B].contiguous().view(-1, 5)[..., 4] # single dimension tensor , shape should be ( S * S - obj_num) * B , curently
-        pre_nega_conf = torch.cat([pre_nega_conf, torch.tensor(extra_conf).cuda()], dim=0)
+        pre_nega_conf = torch.cat([pre_nega_conf, torch.tensor(extra_conf.cuda())], dim=0)
         
-        loss_xy = self.lambda_coord * F.mse_loss(gts[..., : 2], preds_posi[..., : 2], reduction='sum')
-        loss_wh = self.lambda_coord * F.mse_loss(torch.sqrt(gts[..., 2: 4]), torch.sqrt(torch.clamp(preds_posi[..., 2: 4], 0, 1)), reduction='sum')
-        loss_class = F.mse_loss(gt_obj[..., 5 * B : ], pre_obj[..., 5 * B :], reduction='sum')
-        loss_conf_posi = F.mse_loss(gts[..., 4], preds_posi[..., 4], reduction='sum')
-        loss_conf_nega = self.lambda_noobj * F.mse_loss(pre_nega_conf, torch.zeros_like(pre_nega_conf).cuda(), reduction='sum')
+        loss_xy = self.lambda_coord * F.mse_loss(preds_posi[..., : 2], gt_objs[..., : 2], reduction='sum')
+        loss_wh = self.lambda_coord * F.mse_loss(torch.sqrt(torch.clamp(preds_posi[..., 2: 4], 0, 1)), torch.sqrt(gt_objs[..., 2: 4]), reduction='sum')
+        loss_class = F.mse_loss(pred_objs[..., 5 * B :], gt_objs[..., 5 * B : ], reduction='sum')
+        loss_conf_posi = F.mse_loss(preds_posi[..., 4], gt_objs[..., 4], reduction='sum')
+        loss_conf_nega = self.lambda_noobj * F.mse_loss(pre_nega_conf, torch.zeros_like(pre_nega_conf), reduction='sum')
         # # Total loss
         loss = loss_xy + loss_wh + loss_conf_posi +  loss_conf_nega + loss_class
         loss = loss / float(batch_size)
@@ -112,7 +110,6 @@ class Loss(nn.Module):
             'loss_conf_posi': loss_conf_posi,
             'loss_conf_nega': loss_conf_nega
         }
-        # print('my loss',  loss_dict)
         return loss/float(batch_size)
     
 
@@ -120,12 +117,21 @@ class Loss(nn.Module):
         my_gts, my_preds = gts.clone(), predictions.clone()
         batch_size, S, _, N = predictions.size(0), predictions.size(1), predictions.size(2), predictions.size(3) 
         B = (N - self.C) // 5 
-        from utils.tools import create_grid
-        grid = create_grid(S, batch=batch_size).cuda()
+        from origin_yolov1.utils.tools import create_grid
+        grid = create_grid(S, batch=batch_size)
         for b in range(B):
             my_gts[..., 5 *b : 5 * b + 2] = (my_gts[..., 5 *b : 5 * b + 2] + grid) / float(S)
             my_preds[..., 5 *b : 5 * b + 2] = (my_preds[..., 5 *b : 5 * b + 2] + grid)/float(S)
         return my_gts, my_preds
 
 if __name__ == '__main__':
-    pass
+    t = torch.rand(1, 7, 7, 30)
+    t[..., 5: 10] = 0
+    import random
+    for _ in range(5):
+        i = random.randint(0, 6)
+        j = random.randint(0, 6)
+        t[:, j, i, 4] = 1
+    pre = torch.rand(1, 7, 7, 30)
+    myloss = Loss(20)
+    myloss(pre, t)
