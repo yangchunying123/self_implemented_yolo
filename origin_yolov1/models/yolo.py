cabb3f072bf8678bc004581df5a776e2e4b6fbe1
kejian_yangchunying <kejian_yangchunying_ubuntu.com>
2025-03-10 18:27:42 +0800

    after using conv only architecture, output might be infinite, use sigmoid/softmax to normalize output

--- origin_yolov1/models/yolo.py
+++ origin_yolov1/models/yolo.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
 from models.basic import Conv
-from models.backbone.resnet import build_resnet
 from rich import print
+from torchvision.models.resnet import resnet18, ResNet18_Weights
 
 # YOLO
 class YOLOV1(nn.Module):
-    def __init__(self, B = 2, C=20, load_backbone_weight=True):
+    def __init__(self, B=2, C=20):
         super(YOLOV1, self).__init__()
         self.C = C
         self.B = B             
-        # backbone: resnet18
-        self.backbone, feat_dim = build_resnet('resnet18', pretrained=load_backbone_weight)
+        features = resnet18(weights = ResNet18_Weights.IMAGENET1K_V1)
+        self.backbone = nn.Sequential(*list(features.children())[:-2])
+        feat_dim = 512
         # detection head
         self.convsets = nn.Sequential(
             Conv(feat_dim, feat_dim//2, k=1),
@@ -21,6 +22,12 @@ class YOLOV1(nn.Module):
         )
         # pred
         self.pred = nn.Conv2d(feat_dim, 5*B + C, 1)
+        nn.init.constant_(self.pred.bias, 0)  # 其他通道初始化为0
+        # 仅对置信度部分的偏置进行特殊初始化
+        with torch.no_grad():
+            confidence_bias = -torch.log(torch.tensor(99.0))
+            for b in range(self.B):
+                self.pred.bias[:5*b].fill_(confidence_bias)
 
     def forward(self, x):
         # backbone主干网络
@@ -28,65 +35,15 @@ class YOLOV1(nn.Module):
         feat = self.convsets(feat)
         pred = self.pred(feat)
         pred = pred.permute(0, 2, 3, 1).contiguous()
-        for b in range(self.B):
-            pred[..., 5 * b : 5 * b + 2] = torch.sigmoid(pred[..., 5 * b : 5 * b + 2])
-            pred[..., 5 * b + 2 : 5 * b + 4] = torch.exp(pred[..., 5 * b + 2 : 5 * b + 4])
-            pred[..., 5 * b + 4] = torch.sigmoid(pred[..., 5 * b + 4])
-        pred[..., 5 * self.B : ] = torch.sigmoid(pred[..., 5 * self.B: ])
+        pred[..., : 5 * self.B] = torch.sigmoid(pred[..., : 5 * self.B])
+        pred[..., 5 * self.B : ] = torch.softmax(pred[..., 5 * self.B: ], dim=-1)
         return pred
-    
-def load(path):
-    pretrained_state = torch.load(path)
-    myyolo = YOLOV1(load_backbone_weight=False)
-    state_dict_yolo = myyolo.state_dict()
-    
-    # 记录成功加载的键和缺失的键
-    matched_keys = []
-    missing_keys = []
-    
-    # 遍历模型 B 的键，检查是否在模型 A 中存在且形状匹配
-    for key in state_dict_yolo:
-        if key in pretrained_state:
-            # 形状相同则加载权重
-            if pretrained_state[key].shape == state_dict_yolo[key].shape:
-                state_dict_yolo[key] = pretrained_state[key]
-                matched_keys.append(key)
-            else:
-                missing_keys.append(f"{key} (shape mismatch)")
-        else:
-            missing_keys.append(f"{key} (key not found)")
-    
-    # 将更新后的权重加载到模型 B
-    myyolo.load_state_dict(state_dict_yolo)
-    return myyolo, matched_keys, missing_keys
          
 if __name__ == '__main__':
-    # yolo = YOLOV1().cuda()
-    # from datas.voc import VOCDataset, ImageTransfrom
-    # from torch.utils.data import DataLoader
-    # image_dir = '/home/asher/codes/python/self_yolo_imple_serires/DATASET/VOC/2007/JPEGImages'
-    # train_label = '/home/asher/codes/python/self_yolo_imple_serires/DATASET/train.txt'
-    # val_label = '/home/asher/codes/python/self_yolo_imple_serires/DATASET/val.txt'
-
-    # image_size = 416
-    # train_dataset = VOCDataset(image_dir, train_label, image_trans=ImageTransfrom(image_size), grid_size=int(image_size/32))
-    # train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=8)
-
-    # for i, (imgs, targets) in enumerate(train_loader):
-    #     imgs, targets = imgs.cuda(), targets.cuda()
-    #     print(i, imgs.shape)
-    #     preds = yolo(imgs)
-    #     print(preds.shape)
-    
-    # model, match, miss = load('/home/asher/Downloads/yolo_69.6.pth')
-    # model = model.cuda()
-    # t = torch.rand(3, 3, 416, 416)
-    # print(model(t).shape)
-
-    from torchvision.models.resnet import resnet50, ResNet50_Weights
-    backbone = resnet50(weights = ResNet50_Weights.IMAGENET1K_V1)
-    features = nn.Sequential(*list(backbone.children())[:-2])
-
-    t = torch.rand(1, 3, 416, 416)
-    out = features(t)
-    print(out.shape)
+    # t = torch.rand(1, 3, 416, 416)
+    model = YOLOV1()
+    # output = model(t)
+    # print(output[0, 0, 0 , :])
+    # print(torch.sum(output[0,0,0, 10 : ]))
+    for name, param in model.named_parameters():
+        print(name, param.requires_grad)
