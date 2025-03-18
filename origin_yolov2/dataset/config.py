2111f7fff5f72c96028e806247772463413005a0
kejian_yangchunying <kejian_yangchunying_ubuntu.com>
2025-03-18 18:48:48 +0800

    change anchor values

--- origin_yolov2/dataset/config.py
+++ origin_yolov2/dataset/config.py
@@ -9,8 +9,9 @@ yolov2_d19_cfg = {
     'val_size': 416,
     'random_size_range': [13, 13],
     # anchor size
-    'anchor_size_voc': [[1.19, 1.98], [2.79, 4.59], [4.53, 8.92], [8.06, 5.29], [10.32, 10.65]],
+    'anchor_size_voc': [[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]],
     'anchor_size_coco': [[0.53, 0.79], [1.71, 2.36], [2.89, 6.44], [6.33, 3.79], [9.03, 9.74]],
+    'anchor_size_voc_old': [[1.19, 1.98], [2.79, 4.59], [4.53, 8.92], [8.06, 5.29], [10.32, 10.65]],
     # train
     'ignore_thresh': 0.5,
     'class_num': 20,
@@ -20,7 +21,7 @@ yolov2_d19_cfg = {
 yolov2_train_cfg = {
     'batch_size' : 64,
     'lr': 1e-4,
-    'max_epoch': 30,
+    'max_epoch': 80,
     'loss_save_iter' : 5,
-    'loss_save_iter_val': 5
+    'loss_save_iter_val': 100
 }
